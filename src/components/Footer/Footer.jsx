3780dce949c41f0b3f9e1022d8d01adad46c4a6f
akhilsinghrana07 <akhilsinghrana7@gmail.com>
2023-05-07 06:21:30 -0400

    warnings fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
 import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
 import "./Footer.css";
 
